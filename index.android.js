1fd23f2853df003f07f2c8fbc5a45554a71401e6
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-05-13 21:27:41 +0700

    First React Native, Part 1

---
+++ index.android.js
@@ -0,0 +1,54 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+export default class AwesomeProject extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          This is My First App React Native.
+        </Text>
+        <Text style={styles.instructions}>
+          Name :  Rizal Sidik Permana.
+        </Text>
+        <Image source={require('./me.jpg')} style={{width: 200, height:300}}/>
+        <Text style={styles.instructions}>
+          batch : Gagliano.
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
